4e994825fc77760b9f40825f859dec716ce03733
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-07 15:57:45 -0700

    v2 emoticon

--- python/Labs_3/emoticon_gen.py
+++ python/Labs_3/emoticon_gen.py
@@ -2,17 +2,22 @@
 
 import random
 
-counter = 0
-
-while counter < 6:
-    nose_list = ["-", "=", "D"]
-    eyes_list = ["O", ":", ";"]
-    mouths_list = ["P", "C", "<"]
-
-
-    nose = random.choice(nose_list)
-    eyes = random.choice(eyes_list)
-    mouths = random.choice(mouths_list)
-
+def display_face(eyes,nose,mouths):
     random_face = print(f"{eyes}{nose}{mouths}")
-    counter += 1
+    return random_face
+
+def main():
+    counter = 0
+
+    while counter < 6:
+        nose_list = ["-", "=", "~"]
+        eyes_list = ["O", ":", ";"]
+        mouths_list = ["P", "C", "<"]
+        nose = random.choice(nose_list)
+        eyes = random.choice(eyes_list)
+        mouths = random.choice(mouths_list)
+
+        display_face(eyes,nose,mouths)
+        counter += 1
+
+main()
