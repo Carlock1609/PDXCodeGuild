5edeb19cfd91c572f945fdc1de41b2ef47d8b54a
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-06 20:08:58 -0700

    rock_paper_scissor V1

--- python/Labs_3/rock_paper_scissors.py
+++ python/Labs_3/rock_paper_scissors.py
@@ -1,2 +1,36 @@
 #! python3
 # Rock paper scissors game
+
+import random
+
+
+def get_comp_turn():
+    moves = ["Scissors", "Paper", "Rock"]
+    return random.choice(moves)
+
+def get_user_turn():
+    moves = ["Scissors", "Paper", "Rock"]
+    get_turn = input("Please enter in a move: (Rock, Paper or Scissors)").capitalize()
+    while get_turn not in moves:
+        get_user_turn()
+    else:
+        return get_turn
+
+
+def determine_win(user, comp):
+    if user == "Rock" and comp == "Scissors":
+        print("You win!")
+    elif user == "Scissors" and comp == "Paper":
+        print("You win!")
+    elif user == "Paper" and comp == "Rock":
+        print("You win!")
+    elif user == "Rock" and comp == "Paper":
+        print("You lose!")
+    elif user == "Paper" and comp == "Scissors":
+        print("You lose!")
+    elif user == "Scissors" and comp == "Rock":
+        print("You lose!")
+
+print("Welcome to Rock Paper Scissors!")
+determine_win(get_user_turn(), get_comp_turn())
+print(f"The computer picked {get_comp_turn()}!")
