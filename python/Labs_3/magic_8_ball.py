06c5b283a37c4fbdce367c91f32fe183d0df1a7e
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-07 16:12:12 -0700

    v2 magic 8 ball

--- python/Labs_3/magic_8_ball.py
+++ python/Labs_3/magic_8_ball.py
@@ -2,38 +2,28 @@
 
 import random
 
-list = ["It is certain",
-"It is decidedly so",
-"Without a doubt",
-"Yes definitely",
-"You may rely on it",
-"As I see it, yes",
-"Most likely",
-"Outlook good",
-"Yes",
-"Signs point to yes",
-"Reply hazy try again",
-"Ask again later",
-"Better not tell you now",
-"Cannot predict now",
-"Concentrate and ask again",
-"Don't count on it",
-"My reply is no",
-"My sources say no",
-"Outlook not so good",
-"Very doubtful"]
+def gen_response_list():
+    list = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"]
+    return list
 
-while True:
-    print("Welcome to Magic Eight Ball Game!")
+def get_rand_response():
+    return random.choice(gen_response_list())
 
+def main():
     while True:
+        print("Welcome to Magic Eight Ball Game!")
 
-        play_again = input("Would you like to play?").lower()
+        while True:
+            play_again = input("Would you like to play?").lower()
 
-        if play_again == "yes":
-            player_input = input("Please enter in a question: ")
-            print(random.choice(list))
-        else:
-            print("Goodbye!")
-            break
-    break
+            if play_again == "yes":
+                player_input = input("Please enter in a question: ")
+                print(get_rand_response())
+                print()
+            else:
+                print("Goodbye!")
+                break
+        break
+
+main()
