7f7d21f7e7580d235057c0f2175d81b74d8f0a54
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-07 16:25:55 -0700

    working on v3 password gen

--- python/Labs_3/password_gen_1.py
+++ python/Labs_3/password_gen_1.py
@@ -2,52 +2,61 @@
 
 import string, random
 
-lower_case_gen = string.ascii_lowercase
-upper_case_gen = string.ascii_uppercase
-number_list_gen = string.digits
-special_char_list = "!@#$%^&*"
+def get_lower():
+    # Lower loop
+    pass_length_lower = (int(input("Please enter in how many lower_case letters you want: ")) - 1)
 
-password = []
-lower_case = []
-upper_case = []
-numbers_list = []
-special_chars = []
+    lower_case_gen = string.ascii_lowercase
+    lower_case = []
+    lower_case_str = "".join(lower_case)
 
-# make individual functions for each list
+    while len(lower_case) <= pass_length_lower:
+        rand_lower_letter = random.choice(lower_case_gen)
+        lower_case.append(rand_lower_letter)
+    return lower_case_str
 
-# Lower loop
-pass_length_lower = (int(input("Please enter in how many lower_case letters you want: ")) - 1)
+def get_upper():
+    # Upper loop
+    pass_length_upper = (int(input("Please enter in how many upper_case letters you want: ")) - 1)
 
-while len(lower_case) <= pass_length_lower:
-    rand_lower_letter = random.choice(lower_case_gen)
-    lower_case.append(rand_lower_letter)
+    upper_case_gen = string.ascii_uppercase
+    upper_case = []
+    upper_case_str = "".join(upper_case)
 
-# Upper loop
-pass_length_upper = (int(input("Please enter in how many upper_case letters you want: ")) - 1)
+    while len(upper_case) <= pass_length_upper:
+        rand_length_upper = random.choice(upper_case_gen)
+        upper_case.append(rand_length_upper)
+    return upper_case_str
 
-while len(upper_case) <= pass_length_upper:
-    rand_length_upper = random.choice(upper_case_gen)
-    upper_case.append(rand_length_upper)
+def get_nums():
+    # Nums loop
+    pass_length_nums = (int(input("Please enter in how many numbers do you want: ")) - 1)
 
-# Nums loop
-pass_length_nums = (int(input("Please enter in how many numbers do you want: ")) - 1)
+    number_list_gen = string.digits
+    numbers_list = []
+    numbers_list_str = "".join(numbers_list)
 
-while len(numbers_list) <= pass_length_nums:
-    rand_length_nums = random.choice(number_list_gen)
-    numbers_list.append(rand_length_nums)
+    while len(numbers_list) <= pass_length_nums:
+        rand_length_nums = random.choice(number_list_gen)
+        numbers_list.append(rand_length_nums)
+    return numbers_list_str
 
-# Special char loop
-pass_length_special = (int(input("Please enter in how many special characters do you want: ")) - 1)
+def get_chars():
+    # Special char loop
+    pass_length_special = (int(input("Please enter in how many special characters do you want: ")) - 1)
 
-while len(special_chars) <= pass_length_special:
-    rand_length_special = random.choice(special_char_list)
-    special_chars.append(rand_length_special)
+    special_char_list = "!@#$%^&*"
+    special_chars = []
+    special_chars_str = "".join(special_chars)
 
-lower_case_str = "".join(lower_case)
-upper_case_str = "".join(upper_case)
-numbers_list_str = "".join(numbers_list)
-special_chars_str = "".join(special_chars)
+    while len(special_chars) <= pass_length_special:
+        rand_length_special = random.choice(special_char_list)
+        special_chars.append(rand_length_special)
+    return special_chars_str
 
-password = [lower_case_str, upper_case_str, numbers_list_str, special_chars_str]
+def main():
+    password = [get_lower(), get_upper(), get_nums(), get_chars()]
 
-print("".join(password))
+    print("".join(password))
+
+print(get_chars())
