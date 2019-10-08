4ccd462cf02a654c4da4dbe5ba88324c7d44b6f1
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-07 20:27:08 -0700

    v4 unit converter

--- python/Labs_4/unit_converter.py
+++ python/Labs_4/unit_converter.py
@@ -5,31 +5,71 @@
 # 1 m is 1 m
 # 1 km is 1000 m
 
-# v1
+# v4
 
 def get_user_distance():
-    return float((input("What is the distance in meters?: ")))
+    return float((input("What is the distance?: ")))
 
-def get_user_units():
-    get_units = input("What the units?: ")
-    return get_units
+def get_user_input():
+    get_input = input("What is the input?: ")
+    return get_input
+
+def get_user_output():
+    get_output = input("What is the output?: ")
+    return get_output
 
 def converter():
+    ft = 0
     m = 0
-    units = get_user_units()
+    mi = 0
+    km = 0
 
-    if units == "ft":
-        m = 0.3048
+    units_input = get_user_input()
+    units_output = get_user_output()
+
+    # feet to
+    if units_input == "ft" and units_output == "m":
+        ft = 0.3048
+        return ft
+    elif units_input == "ft" and units_output == "km":
+        ft = 3280.84
+        return ft
+    elif units_input == "ft" and units_output == "mi":
+        ft = 5280.0
+        return ft
+
+    # meters to
+    if units_input == "m" and units_output == "ft":
+        m = 3.28084
         return m
-    elif units == "m":
-        m = 1
+    elif units_input == "m" and units_output == "mi":
+        m = 0.000621371
         return m
-    elif units == "mi":
-        m = 1609.34
+    elif units_input == "m" and units_output == "km":
+        m = 0.001
         return m
-    elif units == "km":
-        m = 1000
-        return m
+
+    # miles to
+    if units_input == "mi" and units_output == "ft":
+        mi = 5280
+        return mi
+    elif units_input == "mi" and units_output == "m":
+        mi = 1609.34
+        return mi
+    elif units_input == "mi" and units_output == "km":
+        mi = 1.60934
+        return mi
+
+    # Kilometers to
+    if units_input == "km" and units_output == "ft":
+        km = 3280.84
+        return km
+    elif units_input == "km" and units_output == "m":
+        km = 1000
+        return km
+    elif units_input == "km" and units_output == "mi":
+        km = 0.621371
+        return km
 
 def display():
     return (get_user_distance() * converter())
