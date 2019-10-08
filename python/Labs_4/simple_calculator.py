522ff0421aa06278f1c9bbea84d238d92517d443
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-08 15:33:56 -0700

    v2 simple calc

--- python/Labs_4/simple_calculator.py
+++ python/Labs_4/simple_calculator.py
@@ -21,6 +21,17 @@ def get_operation():
         return get_first_number() / get_second_number()
 
 def main():
-    print(get_operation())
+    run_again = True
+
+    while run_again:
+        print(get_operation())
+
+        play_again = input("Would you like to use calc again?: (Yes or No)").lower()
+
+        if play_again == "yes":
+            run_again = True
+        else:
+            print("Goodbye!")
+            run_again = False
 
 main()
