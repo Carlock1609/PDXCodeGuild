eabb68da3548a7fc88c52b775fa2bdf417b34032
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-07 21:13:52 -0700

    v1 working on calc

--- python/Labs_4/simple_calculator.py
+++ python/Labs_4/simple_calculator.py
@@ -1,1 +1,25 @@
 #! python3
+
+def get_first_number():
+    return int(input("Please enter in a first num: "))
+
+def get_operation():
+    get_user_operation = input("Please enter in a operation: ")
+    if get_user_operation == "-":
+        get_user_operation = -
+    elif get_user_operation == "+":
+        get_user_operation == +
+    elif get_user_operation == "*":
+        get_user_operation == *
+
+def get_second_nmber():
+    return int(input("Please enter in a second num: "))
+
+def display():
+    first_num = get_first_number()
+    operation = get_operation()
+    second_num = get_second_number()
+
+    return first_num + operation + second_num
+
+print(display())
