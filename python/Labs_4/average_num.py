b4afc8ee83d6b52eb22aed1763e58dca6e34527c
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-07 20:53:08 -0700

    v2 average number

--- python/Labs_4/average_num.py
+++ python/Labs_4/average_num.py
@@ -1,10 +1,24 @@
 #! python3
 
-# for num in nums_list:
-#
+# v2
 
-
-def average():
-    nums_list = sum([3,4,5,5,5])
+def average(nums_list):
     return nums_list / 5
-print(average())
+
+def get_user_num():
+    nums_list = []
+    counter = int(input("Please enter how long you would like the number list: "))
+
+    while len(nums_list) < counter:
+        get_num = int(input("Please enter in a number: "))
+        nums_list.append(get_num)
+    return sum(nums_list)
+
+def main():
+    print("You will enter in a length of a list, aswell as the numbers. And then it will find the average of the list of numbers.")
+
+    nums_list = get_user_num()
+
+    print(average(nums_list))
+
+main()
