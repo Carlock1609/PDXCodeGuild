39f4f6bcc5d7e42cafcf3563dfcb3f379eef8add
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-07 19:47:15 -0700

    v2 unit converter

--- python/Labs_4/unit_converter.py
+++ python/Labs_4/unit_converter.py
@@ -1,13 +1,37 @@
 #! python3
 
-# 1 meter = 3.28084 in feet
+# 1 ft is 0.3048 m
+# 1 mi is 1609.34 m
+# 1 m is 1 m
+# 1 km is 1000 m
+
 # v1
 
-def get_user_input():
-    return float((input("Enter in number of feet to be converted to meters: ")))
+def get_user_distance():
+    return float((input("What is the distance in meters?: ")))
 
-def get_meters(feet):
-    meters_to_feet = 0.3048
-    return meters_to_feet * feet
+def get_user_units():
+    get_units = input("What the units?: ")
+    return get_units
 
-print(get_meters(get_user_input()))
+def converter():
+    m = 0
+    units = get_user_units()
+
+    if units == "ft":
+        m = 0.3048
+        return m
+    elif units == "m":
+        m = 1
+        return m
+    elif units == "mi":
+        m = 1609.34
+        return m
+    elif units == "km":
+        m = 1000
+        return m
+
+def display():
+    return (get_user_distance() * converter())
+
+print(display())
