735b964aac460acb035b4d21ff7eaae3e4a0daac
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-08 15:32:51 -0700

    v1 simple calc

--- python/Labs_4/simple_calculator.py
+++ python/Labs_4/simple_calculator.py
@@ -1,25 +1,26 @@
 #! python3
 
+# v2
+
 def get_first_number():
     return int(input("Please enter in a first num: "))
 
-def get_operation():
-    get_user_operation = input("Please enter in a operation: ")
-    if get_user_operation == "-":
-        get_user_operation = -
-    elif get_user_operation == "+":
-        get_user_operation == +
-    elif get_user_operation == "*":
-        get_user_operation == *
-
-def get_second_nmber():
+def get_second_number():
     return int(input("Please enter in a second num: "))
 
-def display():
-    first_num = get_first_number()
-    operation = get_operation()
-    second_num = get_second_number()
+def get_operation():
+    get_user_operation = input("Please enter in a operation: ")
 
-    return first_num + operation + second_num
+    if get_user_operation == "-":
+        return get_first_number() - get_second_number()
+    elif get_user_operation == "+":
+        return get_first_number() + get_second_number()
+    elif get_user_operation == "*":
+        return get_first_number() * get_second_number()
+    elif get_user_operation == "/":
+        return get_first_number() / get_second_number()
 
-print(display())
+def main():
+    print(get_operation())
+
+main()
