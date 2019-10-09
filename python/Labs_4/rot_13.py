05789f9bc7d03c9f92a8b437b73633718b05abf7
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-09 12:03:56 -0700

    v2 rot 13

--- python/Labs_4/rot_13.py
+++ python/Labs_4/rot_13.py
@@ -1,5 +1,5 @@
 #! python3
-# v1
+# v2
 
 import string
 
@@ -18,6 +18,7 @@ def main():
     rot_list = get_rot_list()
     abc_list = get_abc_list()
     encrypt_list = []
+    decrypted_list = []
 
     print("Welcome to rot 13!\nEnter in one letter at a time to be encrypted.")
 
@@ -25,10 +26,11 @@ def main():
 
     while len(encrypt_list) < get_length:
         user_input = get_user_input()
+        decrypted_list.append(user_input)
         encrypt = abc_list.index(user_input)  
         encrypt_list.append(rot_list[encrypt])
     else:
-        print("Your message has been encrypted: " + "".join(encrypt_list))
-        
+        print("\nYour message has been encrypted: " + "".join(encrypt_list))
+        print("Your message decrypted is: " + "".join(decrypted_list))
 
 main()
