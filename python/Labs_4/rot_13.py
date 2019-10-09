a21454032fd8b5ff0381617b2e19cb740b714116
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-08 20:10:52 -0700

    working on v1 rot_13

--- python/Labs_4/rot_13.py
+++ python/Labs_4/rot_13.py
@@ -1,1 +1,17 @@
 #! python3
+# v1
+
+import string
+
+def get_abc_list():
+    abc_list = list(string.ascii_lowercase)
+    return abc_list
+
+def get_rot_list():
+    rot_list = ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+    return rot_list
+
+abc_list = get_abc_list()
+rot_list = get_rot_list()
+
+rot_list = abc_list
