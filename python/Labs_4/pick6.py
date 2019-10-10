be63cb8ac9184114fd74ab8a501d569d3f2f9fd3
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-09 21:04:17 -0700

    v2 pick6 simulation:heart:

--- python/Labs_4/pick6.py
+++ python/Labs_4/pick6.py
@@ -6,18 +6,97 @@ def pick6():
     winning_nums = []
     counter = 1
 
-    while counter <= 5:
-        gen_num = random.randint(1,100)
+    while counter <= 6:
+        gen_num = random.randint(1,2)
         winning_nums.append(gen_num)
         counter += 1
     return winning_nums
 
-def get_user_guesses():
+def sim_user_guesses():
     guesses = []
     counter = 1
 
-    while counter <= 5:
-        get_guesses = int(input("Please enter in 6 numbers: (1-100)"))
+    while counter <= 6:
+        get_guesses = random.randint(1,10)
         guesses.append(get_guesses)
         counter += 1
     return guesses
+
+# user input option
+# def get_user_guesses():
+#     guesses = []
+#     counter = 1
+
+#     while counter <= 6:
+#         get_guesses = int(input("Please enter in 6 numbers: (1-100)"))
+#         guesses.append(get_guesses)
+#         counter += 1
+#     return guesses
+
+def num_matches(user_guesses,winning_nums):
+    winning_tickets_list = 0
+
+    if user_guesses[0] == winning_nums[0]:
+        winning_tickets_list += 1
+    if user_guesses[1] == winning_nums[1]:
+        winning_tickets_list += 1
+    if user_guesses[2] == winning_nums[2]:
+        winning_tickets_list += 1
+    if user_guesses[3] == winning_nums[3]:
+        winning_tickets_list += 1
+    if user_guesses[4] == winning_nums[4]:
+        winning_tickets_list += 1
+    if user_guesses[5] == winning_nums[5]:
+        winning_tickets_list += 1
+    return winning_tickets_list
+
+def get_balance(winning_tickets_list):
+    total_earnings = 0
+
+    if winning_tickets_list == 1:
+        total_earnings += 4
+    elif winning_tickets_list == 2:
+        total_earnings += 7
+    elif winning_tickets_list == 3:
+        total_earnings += 100
+    elif winning_tickets_list == 4:
+        total_earnings += 50000
+    elif winning_tickets_list == 5:
+        total_earnings += 1000000
+    elif winning_tickets_list == 6:
+        total_earnings += 25000000
+    return total_earnings
+
+# user input option
+# add and subtract winnings, keep looping until player done, then print total winnings.
+# def main():
+#     winning_nums = pick6()
+#     user_guesses = get_user_guesses()
+#     total_tickets =  num_matches(user_guesses, winning_nums)
+#     get_balance(total_tickets)
+
+   
+
+def get_run_sim():
+    sim_counter = 0
+    sim_earnings = 0
+    tickets_bought = 0
+
+    while sim_counter < 1000:
+        winning_nums = pick6()
+        user_guesses = sim_user_guesses()
+        total_tickets =  num_matches(user_guesses, winning_nums)
+        run_sim = get_balance(total_tickets)
+        print(f"You made {run_sim} on current ticket picks {user_guesses}, the winning tickets were {winning_nums}\n")
+        sim_counter += 1
+        sim_earnings += run_sim
+        tickets_bought += 2
+    roi = (sim_earnings - tickets_bought)/tickets_bought
+    print(f"You have ${sim_earnings}.00")
+    print(f"If it cost you $2 for every ticket lot, your ROI is {roi}")
+    
+def main():
+   get_run_sim()
+        
+
+main()
