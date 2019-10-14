efac01c879f3c4e748c0bfd24cf2895d0fed6aff
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-13 19:16:19 -0700

    v1 blackjack_advice

--- python/Labs_5/blackjack_advice.py
+++ python/Labs_5/blackjack_advice.py
@@ -4,7 +4,8 @@
 
 # Try a dictionary
 
-deck = {"A": 1,
+def get_player_hand():
+    deck = {"A": 1,
         "2": 2,
         "3": 3, 
         "4": 4, 
@@ -17,28 +18,40 @@ deck = {"A": 1,
         "J": 10, 
         "Q": 10, 
         "K": 10}
-
-# working on making the A value 1 if multiple A's
-
-hand = [1,2,1,1]
-
-if deck["A"] in hand and sum(hand) < 12:
-    hand.append(deck["A"])
-    print(hand)
-    print("yes")
-else:
-    hand.append(deck["A"] - 10)
-    print(hand)
-
-
-
-def get_player_hand():
     player_hand = []
+
     for i in range(3):
-        get_card = input("Please enter in 3 cards to be added to your hand: (A,1,2,3,4,5,6,7,8,9,10,J,Q,K")
+        get_card = deck[input("Please enter in 3 cards to be added to your hand: (A,1,2,3,4,5,6,7,8,9,10,J,Q,K").upper()]
         player_hand.append(get_card)
+        if i == 1 and sum(player_hand) < 11:
+            player_hand.append(11)
     return player_hand
 
 def get_hand_worth(player_hand):
     total_hand = player_hand[0] + player_hand[1] + player_hand[2]
     return total_hand
+
+def main():
+
+    play_again = True
+
+    while play_again:
+        play_again = input("Do you want blackjack advice?: (Yes or No?)").lower()
+        
+        if play_again != "yes":
+            print("Goodbye!")
+            play_again = False
+        else:
+            get_hand = get_hand_worth(get_player_hand())
+            if get_hand == 21:
+                print("Blackjack!")
+            elif get_hand >= 18:
+                print("Stay!")
+            else:
+                print("Hit!")
+
+            if play_again != "yes":
+                play_again = False
+            
+            
+main()
