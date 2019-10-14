32627ac716f6aca2b10078b22b2a32852f239d48
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-13 17:29:34 -0700

    v1 working progress

--- python/Labs_5/blackjack_advice.py
+++ python/Labs_5/blackjack_advice.py
@@ -18,17 +18,27 @@ deck = {"A": 1,
         "Q": 10, 
         "K": 10}
 
+# working on making the A value 1 if multiple A's
+
+hand = [1,2,1,1]
+
+if deck["A"] in hand and sum(hand) < 12:
+    hand.append(deck["A"])
+    print(hand)
+    print("yes")
+else:
+    hand.append(deck["A"] - 10)
+    print(hand)
+
+
 
 def get_player_hand():
     player_hand = []
     for i in range(3):
-        get_card = input("Please enter in 3 cards to be added to your hand: (A,1,2,3,4,5,6,7,8,9,10,J,Q,K)"
+        get_card = input("Please enter in 3 cards to be added to your hand: (A,1,2,3,4,5,6,7,8,9,10,J,Q,K")
         player_hand.append(get_card)
     return player_hand
 
 def get_hand_worth(player_hand):
     total_hand = player_hand[0] + player_hand[1] + player_hand[2]
     return total_hand
-
-
-print(get_hand_worth(get_player_hand()))
