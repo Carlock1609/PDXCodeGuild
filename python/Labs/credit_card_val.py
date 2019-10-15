07d4b9555c8239c7aa602d5879390eb9f5263809
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-15 16:19:15 -0700

    v1 DONE! :beers: wtf are list comprehensions

--- python/Labs/credit_card_val.py
+++ python/Labs/credit_card_val.py
@@ -2,40 +2,36 @@
 
 # v1
 
-# DIFFERENT SOLUTION!! WHY IS IT NOT WORKING
+# LIST COMPREHENSIONS WERE THE ONLY THING THAT WORKED *** 
 
-# Convert the input string into a list of ints
-# Slice off the last digit. That is the check digit.
-# Reverse the digits.
-# Double every other element in the reversed list.
-# Subtract nine from numbers over nine.
-# Sum all values.
-# Take the second digit of that sum.
-# If that matches the check digit, the whole card number is valid.
+def get_digit_dict():
+    credit_card_digits = [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]
+    check_digit = credit_card_digits.pop(15)
+    digit_dict = {"credit_card_digits": credit_card_digits, "check_digit": check_digit}
 
-# How do i split everything into different functions but i need the same digit list
+    return digit_dict
+
+def reverse_digit_list(digit_dict):
+    digit_dict["credit_card_digits"].reverse()
+    return digit_dict
+
+def get_double_digits(digit_dict):
+    digit_dict["credit_card_digits"][::2] = [i*2 for i in digit_dict["credit_card_digits"][::2]]
+    return digit_dict
+
+def get_subtracted_digits(digit_dict):
+    digit_dict["credit_card_digits"] = [i-9 if i > 9 else i for i in digit_dict["credit_card_digits"]]
+    return digit_dict
+
+def get_total_sum(digit_dict):
+    add_total = sum(digit_dict["credit_card_digits"])
+    final_digit = list(str(add_total))
+    if int(final_digit[1]) == digit_dict["check_digit"]:
+        return True
+    else:
+        return False
 
 def main():
-    credit_card_digits = [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]
-    check_digit = []
-    check_digit.append(credit_card_digits.pop(15))
-    reversed_digits = credit_card_digits[::-1]
-    doubled_list = []
-    for i in reversed_digits[1::2]:
-        doubled_list.append(i)
-    for i in reversed_digits[::2]:
-        i *= 2
-        doubled_list.append(i)
-    print(doubled_list)
-        
-# print(main())
-credit_card_digits = [4,5,5,6,7,3,7,5,8,6,8,9,9,8,5,5]
-
-list1 = [10,8,18,9,16,6,16,5,14,3,14,6,10,5,8]
-
-list2 = [8,9,6,5,3,6,5,10,18,16,16,14,14,10,8]
-
-reversed = credit_card_digits[::-1]
-print(reversed)
-
-print(list1 == list2)
+    print(get_total_sum(get_subtracted_digits(get_double_digits(reverse_digit_list(get_digit_dict())))))
+    
+main()
