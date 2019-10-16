d7112bbe50871d86f39a9fae2b4bb0023e25f1bb
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-16 09:57:17 -0700

    different solution

---
+++ python/Labs/number_to_phrase2.py
@@ -0,0 +1,8 @@
+#! python3
+
+#2 
+
+# different solution, try using dict() and also only one input 
+
+def get_user_input():
+    return input("Please enter in a number to be turned into a phrase: (555 -> Five-Hundred and Fifty-Five)")
