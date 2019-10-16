e384cf43c08c504b6a7eecbb7c348298e8151bdb
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-15 21:09:17 -0700

    v1 need to get rid of puncs

--- python/Labs/count_words.py
+++ python/Labs/count_words.py
@@ -1,5 +1,28 @@
 #! python3
 
-open_book = open("C:\\Users\\jcyat\\Desktop\\book.txt", "r")
-list_book = list(open_book)
-print(list_book)
+# v1
+
+
+def get_opened_book():
+    open_book = open("C:\\Users\\jcyat\\Desktop\\book.txt", "r")
+    read_book = open_book.read().replace("'", "")
+    newbook = read_book.lower().split()
+
+    return newbook
+
+def get_word_dict(mylist):
+    word_dict = {}
+    for i in mylist:
+        if i not in word_dict:
+            word_dict.update({i: 1})
+        elif i in word_dict:
+            word_dict[i] += 1
+    return word_dict
+
+
+
+# # word_dict is a dictionary where the key is the word and the value is the count
+# words = list(word_dict.items()) # .items() returns a list of tuples
+# words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+# for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+#     print(words[i])
