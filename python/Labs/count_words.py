be0e9a48b8e184d13b62b26eec342d7250390dd8
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-15 21:23:17 -0700

    v1 figure translate()

--- python/Labs/count_words.py
+++ python/Labs/count_words.py
@@ -8,6 +8,8 @@ def get_opened_book():
     read_book = open_book.read().replace("'", "")
     newbook = read_book.lower().split()
 
+        
+
     return newbook
 
 def get_word_dict(mylist):
@@ -19,7 +21,13 @@ def get_word_dict(mylist):
             word_dict[i] += 1
     return word_dict
 
+def main(word_dict):
+    words = list(word_dict.items())
+    words.sort(key=lambda tup: tup[1], reverse=True)
+    for i in range(min(10, len(words))):
+        print(words[i])
 
+main(get_word_dict(get_opened_book()))
 
 # # word_dict is a dictionary where the key is the word and the value is the count
 # words = list(word_dict.items()) # .items() returns a list of tuples
