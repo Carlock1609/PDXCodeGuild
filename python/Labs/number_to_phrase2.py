eefe4eed4bddd819d008875e6e082522ecaeacb4
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-16 21:13:48 -0700

    Turn everything into single digits and index)

--- python/Labs/number_to_phrase2.py
+++ python/Labs/number_to_phrase2.py
@@ -4,6 +4,8 @@
 
 # different solution, try using dict() and also only one input 
 
+# Dictionary in a dictionary?
+
 def get_user_input():
     user_input = int(input("Please enter in a number to be turned into a phrase: (555 -> Five-Hundred and Fifty-Five)"))
     return user_input
@@ -40,18 +42,12 @@ def get_num_dict(user_input):
                     8: "-Eight",
                     9: "-Nine",}
 
-    if user_input//100 in hundreds_dict:
-        hundreds = hundreds_dict[user_input]
-    if user_input%100 in tens_dict:
-        tens = tens_dict[user_input]
-    if user_input%10 in singles_dict:
-        singles = singles_dict[user_input]
-    print(f"{hundreds}{tens}{singles}")
+
+# get_num_dict(get_user_input())
 
 def main():
     pass
 
-# get_num_dict(get_user_input())
 
 hundreds_dict = {100: "One-Hundred",
                 200: "Two-Hundred",
@@ -63,8 +59,26 @@ hundreds_dict = {100: "One-Hundred",
                 800: "Eight-Hundred",
                 900: "Nine-Hundred",}
 
-num = 500
+tens_dict = {10: "and Ten",
+            20: "and Twenty",
+            30: "and Thirty",
+            40: "and Forty",
+            50: "and Fifty",
+            60: "and Sixty",
+            70: "and Seventy",
+            80: "and Eighty",
+            90: "and Ninety",}
 
-if num//100 in hundreds_dict:
-    hundreds = hundreds_dict[num]
-print(hundreds)
+singles_dict = {1: "-One",
+                2: "-Two",
+                3: "-Three",
+                4: "-Four",
+                5: "-Five",
+                6: "-Six",
+                7: "-Seven",
+                8: "-Eight",
+                9: "-Nine",}
+
+
+
+print(hundreds_dict[num1 // 10])
