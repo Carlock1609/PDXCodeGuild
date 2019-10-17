12dfaba7785df3fd6ca43c545688c16db406eb6f
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-17 16:28:20 -0700

    v4 updated solution

--- python/Labs/unit_converter.py
+++ python/Labs/unit_converter.py
@@ -1,71 +1,30 @@
 #! python3
-# v4
+
+# v4 updated solution
 
 def get_user_distance():
     return float((input("What is the distance?: ")))
 
 def get_user_input():
-    get_input = input("What is the input?: ")
-    return get_input
+    user_input = input("What measurement type would you like convert?: (inchs, feet, yards, meters, mils, kilometers)")
+    return user_input
 
 def get_user_output():
-    get_output = input("What is the output?: ")
-    return get_output
+    user_output = input("What measurement would you like to get back?: ")
+    return user_output
 
-def converter():
-    ft = 0
-    m = 0
-    mi = 0
-    km = 0
+def main():
+    measure_dict = {   "inch": 0.0254,
+                "feet": 0.3048,
+                "yards": 0.9144,
+                "meters": 1,
+                "miles": 1609.34,
+                "kilometers": 1000,
+    }
 
-    units_input = get_user_input()
-    units_output = get_user_output()
-
-    # feet to
-    if units_input == "ft" and units_output == "m":
-        ft = 0.3048
-        return ft
-    elif units_input == "ft" and units_output == "km":
-        ft = 3280.84
-        return ft
-    elif units_input == "ft" and units_output == "mi":
-        ft = 5280.0
-        return ft
-
-    # meters to
-    if units_input == "m" and units_output == "ft":
-        m = 3.28084
-        return m
-    elif units_input == "m" and units_output == "mi":
-        m = 0.000621371
-        return m
-    elif units_input == "m" and units_output == "km":
-        m = 0.001
-        return m
-
-    # miles to
-    if units_input == "mi" and units_output == "ft":
-        mi = 5280
-        return mi
-    elif units_input == "mi" and units_output == "m":
-        mi = 1609.34
-        return mi
-    elif units_input == "mi" and units_output == "km":
-        mi = 1.60934
-        return mi
-
-    # Kilometers to
-    if units_input == "km" and units_output == "ft":
-        km = 3280.84
-        return km
-    elif units_input == "km" and units_output == "m":
-        km = 1000
-        return km
-    elif units_input == "km" and units_output == "mi":
-        km = 0.621371
-        return km
-
-def display():
-    return (get_user_distance() * converter())
-
-print(display())
+    get_distance = get_user_distance()
+    user_input = get_user_input()
+    user_output = get_user_output()
+    print(get_distance*((measure_dict[user_input])/measure_dict[user_output]))
+    
+main()
