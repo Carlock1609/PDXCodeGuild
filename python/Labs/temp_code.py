4dd3fa9ae7aec728dad4c1f88a81967a828fc0a5
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-16 23:50:08 -0700

    almost there :beers:

---
+++ python/Labs/temp_code.py
@@ -0,0 +1,53 @@
+#! python3
+
+# FILE USED JUST FOR PRACTICE CODE
+def get_user_input():
+    user_input = int(input("Please enter in a number to be turned into a phrase: (555 -> Five-Hundred and Fifty-Five)"))
+    return user_input
+
+hundreds_dict = {1: "One-Hundred",
+                    2: "Two-Hundred",
+                    3: "Three-Hundred",
+                    4: "Four-Hundred",
+                    5: "Five-Hundred",
+                    6: "Six-Hundred",
+                    7: "Seven-Hundred",
+                    8: "Eight-Hundred",
+                    9: "Nine-Hundred",
+                    }
+
+tens_dict1 = {1: "and Ten",
+                2: "and Twenty",
+                3: "and Thirty",
+                4: "and Forty",
+                5: "and Fifty",
+                6: "and Sixty",
+                7: "and Seventy",
+                8: "and Eighty",
+                9: "and Ninety",
+                }
+
+tens_dict2 = {1: "Eleven",
+                2: "Twelve",
+                3: "Thirteen",
+                4: "Fourteen",
+                5: "Fifteen",
+                6: "Sixteen",
+                7: "Seventeen",
+                8: "Eighteen",
+                9: "Nineteen",
+                }
+
+singles_dict = {1: "-One",
+                    2: "-Two",
+                    3: "-Three",
+                    4: "-Four",
+                    5: "-Five",
+                    6: "-Six",
+                    7: "-Seven",
+                    8: "-Eight",
+                    9: "-Nine",
+                    }
+
+
+print(tens_dict1[(get_user_input%100)%10])
