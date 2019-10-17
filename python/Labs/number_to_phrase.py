da5cb3b4d358187cd1d369a23fed9a672ad1ccc5
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-17 08:24:13 -0700

    v2 updated solution and done :beers:

--- python/Labs/number_to_phrase.py
+++ python/Labs/number_to_phrase.py
@@ -1,94 +1,68 @@
 #! python3
 
-# v2
-# HOW DO I GET IT TO DO TENS!
+# v2 UPDATED!
 
 def get_user_input():
-    return int(input("Please enter in the same number THREE TIMES to be converted into phrase: \n(550 -> Five-Hundred and Fifty-Five)"))
+    user_input = int(input("Please enter in a number to be turned into a phrase: (555 -> Five-Hundred and Fifty-Five)"))
+    return user_input
 
-def get_hundreds(get_num):
-    if get_num > 100:
-        return get_num//100
+def get_num_tuple(user_input):
+    hundreds_dict = {1: "One-Hundred",
+                        2: "Two-Hundred",
+                        3: "Three-Hundred",
+                        4: "Four-Hundred",
+                        5: "Five-Hundred",
+                        6: "Six-Hundred",
+                        7: "Seven-Hundred",
+                        8: "Eight-Hundred",
+                        9: "Nine-Hundred",
+                        }
 
-def get_tens(get_num):
-    if get_num > 100:
-        return get_num%100
+    tens_dict1 = {  2: "and Twenty",
+                    3: "and Thirty",
+                    4: "and Forty",
+                    5: "and Fifty",
+                    6: "and Sixty",
+                    7: "and Seventy",
+                    8: "and Eighty",
+                    9: "and Ninety",
+                    }
 
-def get_singles(get_num):
-    if get_num > 100:
-        return get_num%10
+    tens_dict2 = {  0: "Ten",
+                    1: "Eleven",
+                    2: "Twelve",
+                    3: "Thirteen",
+                    4: "Fourteen",
+                    5: "Fifteen",
+                    6: "Sixteen",
+                    7: "Seventeen",
+                    8: "Eighteen",
+                    9: "Nineteen",
+                    }
 
-def convert_hundreds(get_hundreds):
-    list_hundreds = ["One-Hundred", "Two-Hundred", "Three-Hundred", "Four-Hundred", "Fifty-Hundred", "Sixty-Hundred", "Seven-Hundred", "Eight-Hundred", "Nine-Hundred"]
-
-    if get_hundreds >= 1 and get_hundreds < 2:
-        return list_hundreds[0]
-    if get_hundreds >= 2 and get_hundreds < 3:
-        return list_hundreds[1]
-    if get_hundreds >= 3 and get_hundreds < 4:
-        return list_hundreds[2]
-    if get_hundreds >= 4 and get_hundreds < 5:
-        return list_hundreds[3]
-    if get_hundreds >= 5 and get_hundreds < 6:
-        return list_hundreds[4]
-    if get_hundreds >= 6 and get_hundreds < 7:
-        return list_hundreds[5]
-    if get_hundreds >= 7 and get_hundreds < 8:
-        return list_hundreds[6]
-    if get_hundreds >= 8 and get_hundreds < 9:
-        return list_hundreds[7]
-    if get_hundreds >= 9 and get_hundreds < 10:
-        return list_hundreds[8]
-
-def convert_tens(get_tens):
-    list_tens = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-
-    if get_tens >= 20 and get_tens < 30:
-        return list_tens[0]
-    if get_tens >= 30 and get_tens < 40:
-        return list_tens[1]
-    if get_tens >= 40 and get_tens < 50:
-        return list_tens[2]
-    if get_tens >= 50 and get_tens < 60:
-        return list_tens[3]
-    if get_tens >= 60 and get_tens < 70:
-        return list_tens[4]
-    if get_tens >= 70 and get_tens < 80:
-        return list_tens[5]
-    if get_tens >= 80 and get_tens < 90:
-        return list_tens[6]
-    if get_tens >= 90 and get_tens < 100:
-        return list_tens[7]
-    
-
-def convert_singles(get_singles):
-    list_singles = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-
-    if get_singles == 0:
-        return list_singles[0]
-    if get_singles == 1:
-        return list_singles[1]
-    if get_singles == 2:
-        return list_singles[2]
-    if get_singles == 3:
-        return list_singles[3]
-    if get_singles == 4:
-        return list_singles[4]
-    if get_singles == 5:
-        return list_singles[5]
-    if get_singles == 6:
-        return list_singles[6]
-    if get_singles == 7:
-        return list_singles[7]
-    if get_singles == 8:
-        return list_singles[8]
-    if get_singles == 9:
-        return list_singles[9]
+    singles_dict = {1: "One",
+                        2: "Two",
+                        3: "Three",
+                        4: "Four",
+                        5: "Five",
+                        6: "Six",
+                        7: "Seven",
+                        8: "Eight",
+                        9: "Nine",
+                        }
+    # If user input is greater than 320 --> print each dict
+    if user_input%100 > 20:
+        print(f"{hundreds_dict[user_input//100]} {tens_dict1[(user_input//10)%10]} {singles_dict[user_input%10]}")
+    # If user input is less than 320 and greater than 310 --> print hundreds and tens dict
+    elif user_input%100 < 20 and user_input%100 >= 10:
+        print(f"{hundreds_dict[user_input//100]} and {tens_dict2[user_input%10]}")
+    # If user input is less than 10 and greater than 1 --> print hundreds and singles dict
+    elif user_input%100 < 10 and user_input%100 >= 1:
+        print(f"{hundreds_dict[user_input//100]} and {singles_dict[user_input%10]}")
+    # If user input is an even hundred --> print hundreds dict
+    elif user_input%100 == 0:
+        print(f"{hundreds_dict[user_input//100]}")
 
 def main():
-    hundreds = convert_hundreds(get_hundreds(get_user_input()))
-    tens = convert_tens(get_tens(get_user_input()))
-    singles = convert_singles(get_singles(get_user_input()))
-    print(f"{hundreds} and {tens}-{singles}")
-
+    get_num_tuple(get_user_input())
 main()
