5ddf32534fb1f340824bd3546289bc1100225f0f
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-17 17:03:16 -0700

    v2 updating solution

--- python/Labs/simple_calculator.py
+++ python/Labs/simple_calculator.py
@@ -1,6 +1,6 @@
 #! python3
 
-# v2
+# v2 updated solution
 
 def get_first_number():
     return int(input("Please enter in a first num: "))
@@ -9,29 +9,15 @@ def get_second_number():
     return int(input("Please enter in a second num: "))
 
 def get_operation():
-    get_user_operation = input("Please enter in a operation: ")
+   operation = input("Please enter in an operation: ")
 
-    if get_user_operation == "-":
-        return get_first_number() - get_second_number()
-    elif get_user_operation == "+":
-        return get_first_number() + get_second_number()
-    elif get_user_operation == "*":
-        return get_first_number() * get_second_number()
-    elif get_user_operation == "/":
-        return get_first_number() / get_second_number()
+   operation_dict = {   "+": +,
+                        "-": -,
+                        "*": *,
+                        "/": /, 
+                    }
+    print(operation_dict[operation])
 
 def main():
-    run_again = True
-
-    while run_again:
-        print(get_operation())
-
-        play_again = input("Would you like to use calc again?: (Yes or No)").lower()
-
-        if play_again == "yes":
-            run_again = True
-        else:
-            print("Goodbye!")
-            run_again = False
-
-main()
+    pass
+get_operation()
