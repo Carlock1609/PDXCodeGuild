e44841f7b117a2b63790379f6ff090da5577c6bc
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-17 21:14:26 -0700

    Practice strings

---
+++ practice_fundamentals/string_practice.py
@@ -0,0 +1,33 @@
+#! python3
+
+# def add(a, b):
+#     return a + b
+# print(add(2,2))
+
+
+# Problem 1
+# Get a string from the user, print out another string, doubling every letter.
+
+
+# Problem 2
+# Write a function that takes a string, and returns a list of strings, each missing a different character.
+
+
+# Problem 3
+# Return the letter that appears the latest in the english alphabet.
+
+
+# Problem 4
+# Write a function that returns the number of occurances of 'hi' in a given string.
+
+
+# Problem 5
+# Write a function that returns True if a given string contains the same number of 'cat' as it does 'dog'
+
+
+# Problem 6
+# Return the number of letter occurances in a string.
+
+
+# Problem 7
+# Convert input strings to lowercase without any surrounding whitespace.
