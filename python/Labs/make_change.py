acb0aac2d6ffad775edea490f9e43bd19c990137
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-18 19:54:35 -0700

    v2 updated solution make_change

--- python/Labs/make_change.py
+++ python/Labs/make_change.py
@@ -2,16 +2,20 @@
 
 # v2
 
-# Add update change
-
-def get_user_input():
-    return int(input("Please enter in a dollar amount to be converted to pennies: (150 or 4.50)"))
-
-def convert_pennies(change):
-    return str(int(change * 100))
+# issue with pennies. Always one short
 
 def main():
-    get_change = get_user_input()
-    print(f"{get_change}.00 dollars converted to pennies is {convert_pennies(get_change)} pennies.")
+    get_user_input = float(input("Please enter in a float dollar amount to be converted to pennies: (150.05 or 4.50)"))
+
+    quarters = (get_user_input//.25) 
+    quarters_remainder = (get_user_input%.25)
+    dimes = (quarters_remainder//.10)
+    dimes_remainder = (quarters_remainder%.10)
+    nickels = (dimes_remainder//.05)
+    nickels_remainder = (dimes_remainder%.05)
+    pennies = (nickels_remainder//.01)
+    pennies_remainder = (nickels_remainder%.01)
+
+    print(f"{quarters} quarters, {dimes} dimes, {nickels} nickels, {pennies} pennies")
 
 main()
