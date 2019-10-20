4ea559cf1b1c03928cdbc1a2c6cc1eaa936243cc
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-20 12:38:04 -0700

    last digit done, figure out first and second

--- python/Labs/peaks_and_valleys.py
+++ python/Labs/peaks_and_valleys.py
@@ -2,18 +2,21 @@
 
 # v1
 
-# find solution for the first iteration and how to skip it on the for loop
-# and the last
+# figured out how to get last digit, now just get the first digit
+# I dont think data[i-1] will work because it still doesnt know indices
 
 def get_data():
-    data = [6,5,1,1,1,3,1,1,1,9,1,1,7,1,9,1,1,1,8,]
+    data = [6,5,1,1,1,3,1,1,1,9,1,1,7,9,1,1,8,]
+
     return data
 
 def get_peaks(data):
     peaks_list = []
 
     for i in data:
-        if i == data[0] and data[i+1] < i:
+        if i == data[0] and data[1] < i:
+            peaks_list.append(i)
+        elif i == data[::-1][0] and data[::-1][1] < i:
             peaks_list.append(i)
         else:
             if data[i-1] < i and data[i+1] < i:
@@ -33,4 +36,4 @@ def get_valleys(data):
 #     print(peaks_and_valleys_list)
 
 print(get_peaks(get_data()))
-
+print(get_data())
