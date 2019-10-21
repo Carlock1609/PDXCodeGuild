c4214f0f84cb58b2b967f3d9be92a769a64c7d8b
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-21 16:15:30 -0700

    Index tables

--- python/Labs/peaks_and_valleys.py
+++ python/Labs/peaks_and_valleys.py
@@ -3,36 +3,40 @@
 # v1
 
 # figured out how to get last digit, now just get the first digit
-# I dont think data[i-1] will work because it still doesnt know indices
+# I dont think data[i-1] will work because it still doesnt know the index
+# try using index to check for it in the list to compare
+# use two tables? -> one with the index list and one with the data list. and compare over eachother
+# Top is data, bottom is index. check data retain the index. 
 
 def get_data():
-    data = [4,5,1,1,1,3,1,1,1,9,1,1,7,9,1,1,2,]
+    data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
     return data
 
 def get_peaks(data):
-    peaks_list = []
+    peaks = []
+    index_list = list(range(len(data)))
 
-    for i in data:
-        if i == data[0] and data[0] > data[1]:
-            peaks_list.append(i)
-        elif i == data[::-1][0] and data[::-1][1] < i:
-            peaks_list.append(i)
-        else:
-            if data[i-1] < i and data[i+1] < i:
-                peaks_list.append(i)
-    return peaks_list
+    for i in index_list:
+        if (i-1) in index_list and (i+1) in index_list: # makes the index start at 1 so the indexing is not out of range?
+            if data[i] >= data[i-1] and data[i] >= data[i+1]:
+                peaks.append(i)
+    return peaks
 
 def get_valleys(data):
-    pass
-    
+    valleys = []
+    index_list = list(range(len(data)))
 
-# def main():
-#     peaks = get_peaks(get_data())
-#     valleys = get_valleys(get_data())
-#     peaks_and_valleys_list = []
-#     peaks_and_valleys_list.append(peaks)
-#     peaks_and_valleys_list.append(valleys)
-#     print(peaks_and_valleys_list)
+    for i in index_list:
+        if (i-1) in index_list and (i+1) in index_list: # makes the index start at 1 so the indexing is not out of range?
+            if data[i] <= data[i-1] and data[i] <= data[i+1]:
+                valleys.append(i)
+    return valleys
 
-print(get_peaks(get_data()))
-print(get_data())
+def peaks_and_valleys(peaks, valleys):
+    return peaks+valleys
+
+def main():
+    peaks = get_peaks(get_data())
+    valleys = get_valleys(get_data())
+    print(peaks_and_valleys(peaks, valleys))
+main()
