90592ff604cde429c95f0beb97b358b7867da2c1
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-21 17:57:15 -0700

    moving dir

--- practice_fundamentals/problem_2.py
+++
@@ -1,9 +0,0 @@
-#! python3
-
-def opposite(a,b):
-    if a < 0 and b > 0:
-        return True
-    if a > 0 and b < 0:
-        return True
-
-print(opposite(2,-2))
--- practice_fundamentals/writing_reading.py
+++
@@ -1,4 +0,0 @@
-#! python3
-
-with open("phonebook.txt", "a") as phone_book_list:
-    contents = phone_book_list.write("Hello world!")
