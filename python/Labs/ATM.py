675d0acf6cefa40342928f537703638dfbf0844b
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-22 19:30:19 -0700

    DONE! :beers: :beers: :beers: :beers: :beers:

--- python/Labs/ATM.py
+++ python/Labs/ATM.py
@@ -5,66 +5,54 @@
 class ATM():
     def __init__(self, 
                 balance=0,
+                print_transactions=[],
                 ):
         self.balance = balance
+        self.print_transactions = []
 
     def check_balance(self):
-        total_balance = 0
-        return total_balance
+        return self.balance
 
-    def get_deposit(self):
-        amount = int(input("Please enter in a dollar amount: "))
-        return f"You deposited ${amount}"
+    def get_deposit(self, amount):
+        self.balance += amount
+        self.print_transactions.append(f"You deposited ${amount}")
+        return self.balance
     # Use this 
-    def check_withdrawel(self,amount):
-        if amount >= self.balance:
-            return False
-        else:
-            return True
+    def check_withdrawel(self, amount):
+        return amount > self.balance
 
-    def get_withdraw(self):
-        amount = int(input("Please enter in a dollar amount: "))
-        return f"You withdrew ${amount}"
+    def get_withdraw(self, amount):
+        self.balance -= amount
+        self.print_transactions.append(f"You withdrew ${amount}")
+        return self.balance
 
-    def print_transactions(self):
-        trans_list = []
-        return trans_list
+    def get_transactions(self,):
+        return self.print_transactions
+
+def get_amount():
+    amount = int(input("Please enter in a dollar amount: "))
+    return amount
 
 def main():
-    my_atm = ATM(balance=1500)
+    my_atm = ATM(balance=1500,
+                print_transactions=[])
 
-    trans = my_atm.print_transactions()
-    balance = my_atm.check_balance()
-    # figuring out how to get mutlple inputs to add to trans
-    # why is the user input AMOUNT being called everytime, and functions fail without it
-    # while True:
-    #     amount = int(input("Please enter in a dollar amount: "))
-
-    # MAKE LOOP WITH AMOUNT IN EACH IF STATEMENT
-    # LOOK UP HOW TO STRUCTURE THIS MESS
-    
     while True:
         user_input = input("What would you like to do (Deposit, Withdraw, Check Balance, History): ").lower()
 
         if user_input == "deposit":
-            deposit = my_atm.get_deposit()
-            print(f"\n{deposit}")
-            print(trans.append(deposit))
+            my_atm.get_deposit(get_amount())
         elif user_input == "withdraw":
-            withdraw = my_atm.get_withdraw()
-            print(f"\n{withdraw}")
-            print(trans.append(withdraw))
+            amount = get_amount()
+            if my_atm.check_withdrawel(amount) == False:
+                my_atm.get_withdraw(amount)
+            else:
+                print("You do not have enough money. Please feed me more dollars.")
         elif user_input == "check balance":
-            print(f"Your total balance is ${balance}")
-            print(trans.append(balance))
+            print(f"Your total balance is ${my_atm.check_balance()}")
+            print((my_atm.check_balance()))
         elif user_input == "history":
-            print(f"Your transactions are {trans}")
+            print(f"Your transactions are {my_atm.get_transactions()}")
         else:
             print("\nPlease enter in Deposit, Withdraw, Check Balance or History: ")
-            print(user_input)
-
-
-
 main()
-
-# print(trans.append(deposit)) THIS WORKS FOR ADDING TO LIST
