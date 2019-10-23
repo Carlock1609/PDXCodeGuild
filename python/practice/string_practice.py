6b7cb2d3f15f384eca6faed55c6ad02491f16f7c
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-10-23 16:23:06 -0700

    String Practice done :beers:

--- python/practice/string_practice.py
+++ python/practice/string_practice.py
@@ -1,19 +1,12 @@
 #! python3
 
-import string
-
-# def add(a, b):
-#     return a + b
-# print(add(2,2))
-
-
-# Problem 1
+# Problem 1 Done** 
 # Get a string from the user, print out another string, doubling every letter.
 def problem_1():
     user_input = "hello"
     [print(i*2, end="") for i in user_input]
 
-# Problem 2
+# Problem 2 Done**
 # Write a function that takes a string, and returns a list of strings, each missing a different character.
 def problem_2(word):
     list1 = []
@@ -22,33 +15,41 @@ def problem_2(word):
     print(list1)
 # problem_2("kitten")
 
-# Problem 3
+# Problem 3 Done**
 # Return the letter that appears the latest in the english alphabet.
 # IMPLEMENT .sort() METHOD PLEASE
-def latest_letter(letters):
-    get_list = letters.split()
-    for i in get_list:
-        print(get_list.sort())
+def latest_letter(letters): 
+    list1 = list(letters)
+    list1.sort()
+    return list1[-1]
+# print(latest_letter("pneumonoultrazmicroscopicsilicovolcanoconiosixys"))
 
-
-latest_letter("pneumonoultramicroscopicsilicovolcanoconiosixys")
-
-# Problem 4
+# Problem 4 Done**
 # Write a function that returns the number of occurances of 'hi' in a given string.
 def count_hi(word):
-    counter = 0
-    if word.find("hi"):
-        counter += 1
-    print(counter)
-# count_hi("hihi")
+    return word.count("hi")
+# print(count_hi("hihihi"))
 
-# Problem 5
+# Problem 5 Done**
 # Write a function that returns True if a given string contains the same number of 'cat' as it does 'dog'
+def cat_dog(word):
+    dog_count = word.count("dog")
+    cat_count = word.count("cat")
+    return dog_count == cat_count     
+# print(cat_dog("catdogcatdog"))
 
-
-# Problem 6
+# Problem 6 Done**
 # Return the number of letter occurances in a string.
+def count_letter(letter, word):
+    return word.count(letter)
+# count_letter('i', 'antidisestablishmentterianism')
+# count_letter('p', 'pneumonoultramicroscopicsilicovolcanoconiosis')
 
-
-# Problem 7
+# Problem 7 Done**
 # Convert input strings to lowercase without any surrounding whitespace.
+def lower_case(word):
+    lower_word = word.lower()
+    split_word = lower_word.split()
+    return "".join(split_word)
+# print(lower_case("SUPER!"))
+# print(lower_case("        NANNANANANA BATMAN        "))
