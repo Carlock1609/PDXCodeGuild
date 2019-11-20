de8d23f2646debbf970725f334840b95d38360e3
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-19 23:28:57 -0800

    Rot13 V2 DONE! :beers:

--- js/Labs/rot13.js
+++ js/Labs/rot13.js
@@ -1,7 +1,5 @@
 const readline = require("readline-sync");
 
-// DONE!
-
 function main() {
     let rotation = readline.question("Enter in a rotation: ");
     let encrypt = readline.question("Enter in a msg to be encrypted: ");
@@ -9,10 +7,10 @@ function main() {
     let strLow = "abcdefghijklmnopqrstuvwxyz";
     let strUpp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    let firstHalfLow = strLow.slice(0, rotation % 27);
-    let secondHalfLow = strLow.slice(rotation % 27, strLow.length);
-    let firstHalfUpp = strUpp.slice(0, rotation % 27);
-    let secondHalfUpp = strUpp.slice(rotation % 27, strUpp.length);
+    let firstHalfLow = strLow.slice(0, rotation % 26);
+    let secondHalfLow = strLow.slice(rotation % 26, strLow.length);
+    let firstHalfUpp = strUpp.slice(0, rotation % 26);
+    let secondHalfUpp = strUpp.slice(rotation % 26, strUpp.length);
 
     let rotatedLow = `${secondHalfLow}${firstHalfLow}`;
     let rotatedUpp = `${secondHalfUpp}${firstHalfUpp}`;
