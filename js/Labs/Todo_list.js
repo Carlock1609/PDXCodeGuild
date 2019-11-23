c557af1ca0e5ebf1d8a5ff448c1b268a96ad22fe
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-23 12:47:31 -0800

    finished strike, figure out remove

--- js/Labs/Todo_list.js
+++ js/Labs/Todo_list.js
@@ -1,6 +1,9 @@
 let addEle = document.getElementById("add")
 let remEle = document.getElementById("remove")
-let strikeEle = document.getElementById("todo-item")
+let strikeEle = document.getElementById("todos")
+
+let counter = 0
+
 
 addEle.addEventListener("click", event => {
     let newList = document.getElementById("todos")
@@ -8,17 +11,37 @@ addEle.addEventListener("click", event => {
     let newEle = document.createElement("li")
 
     newEle.appendChild(document.createTextNode(userInput.value))
-    newEle.setAttribute("id", "todo-item")
+    newEle.setAttribute("id", `list-item${counter}`)
+    newEle.setAttribute("onclick", `lineThrough('list-item${counter}', '${userInput.value}')`)
     newList.appendChild(newEle)
+    counter += 1
 })
 
-strikeEle.addEventListener("click", event => {
-    strikeEle.style.textDecoration = "line-through"
-})
+function lineThrough(item, userInput) {
+    let result = userInput.strike()    
+    document.getElementById(`${item}`).innerHTML = result
 
+}
 
+let btn = document.getElementById('testbtn');
+btn.addEventListener('click', function(blargle){
+    print_event(blargle);
+});
 
+function print_event(blargle){
+    console.log(blargle);
 
+    event.target.remove();
+}
+
+// x.addEventListener("click", lineThrough => {
+//     x.style.color = "pink"
+// })
+
+// function lineThrough() {
+//     let x = document.getElementsByTagName("li")
+//     x.style.color = "pink"
+// }
 
 // wait to edit until click on add and it moves over
 // remEle.addEventListener("click", event => {
