ef46bba1d88ca9fe9fc8d616d2ec0b5e7e53ad9e
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-26 00:02:10 -0800

    finished bootstrap/css/html

--- js/Labs/redirector.js
+++ js/Labs/redirector.js
@@ -1,14 +1,15 @@
-let redirect = document.querySelector("a")
-
-redirect.addEventListener("click", function() {
-    redirect.setAttribute("href", getRandomLink())
-})
-
 
 function getRandomLink() {
     let links = ["https://www.google.com/", "https://www.bing.com/", "https://www.facebook.com/", "https://unsplash.com/", "https://fonts.google.com/", "https://www.codewars.com/dashboard", "https://www.codewars.com/dashboard", "https://colours.neilorangepeel.com/"]
     return links[Math.floor(Math.random() * 8)] 
 }
 
+function initiateTimer() {
+    let redirect = document.querySelector("a");
 
-let x = window.setTimeout(getRandomLink(), 9000)
+    redirect.addEventListener("click", function() {
+        redirect.setAttribute("href", getRandomLink());
+    });
+};
+
+setTimeout(initiateTimer(), 5000);
