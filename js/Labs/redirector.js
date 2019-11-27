84d2c76607df095bf583f59170067410c48649c2
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-26 18:28:43 -0800

    updates

--- js/Labs/redirector.js
+++ js/Labs/redirector.js
@@ -1,15 +1,15 @@
+let redirect = document.querySelector("a");
 
 function getRandomLink() {
-    let links = ["https://www.google.com/", "https://www.bing.com/", "https://www.facebook.com/", "https://unsplash.com/", "https://fonts.google.com/", "https://www.codewars.com/dashboard", "https://www.codewars.com/dashboard", "https://colours.neilorangepeel.com/"]
-    return links[Math.floor(Math.random() * 8)] 
+    let links = ["https://www.google.com/", "https://www.bing.com/", "https://www.facebook.com/", "https://unsplash.com/", "https://fonts.google.com/", "https://www.codewars.com/dashboard", "https://www.codewars.com/dashboard", "https://colours.neilorangepeel.com/"];
+    return links[Math.floor(Math.random() * 8)]; 
 }
 
-function initiateTimer() {
-    let redirect = document.querySelector("a");
 
-    redirect.addEventListener("click", function() {
-        redirect.setAttribute("href", getRandomLink());
-    });
+function initiateTimer() {
+    redirect.setAttribute("href", getRandomLink());
 };
 
-setTimeout(initiateTimer(), 5000);
+redirect.addEventListener("click", function() {
+    setTimeout(initiateTimer(), 6000);
+})
