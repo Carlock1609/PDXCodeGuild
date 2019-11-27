ee86692498d2d8f6bbd43a49d48e5489a0063902
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-26 20:04:46 -0800

    redirector done, maybe add count down? nah

--- js/Labs/redirector.js
+++ js/Labs/redirector.js
@@ -1,15 +1,11 @@
 let redirect = document.querySelector("a");
 
-function getRandomLink() {
-    let links = ["https://www.google.com/", "https://www.bing.com/", "https://www.facebook.com/", "https://unsplash.com/", "https://fonts.google.com/", "https://www.codewars.com/dashboard", "https://www.codewars.com/dashboard", "https://colours.neilorangepeel.com/"];
-    return links[Math.floor(Math.random() * 8)]; 
+function redirector() {
+    let links = ["https://www.google.com/", "https://www.bing.com/", "http://www.runescape.com/", "https://unsplash.com/", "https://fonts.google.com/", "https://www.codewars.com/dashboard", "https://www.codewars.com/dashboard", "https://colours.neilorangepeel.com/"];
+    link = links[Math.floor(Math.random() * 8)]
+    window.location.href = link;
 }
 
-
-function initiateTimer() {
-    redirect.setAttribute("href", getRandomLink());
-};
-
 redirect.addEventListener("click", function() {
-    setTimeout(initiateTimer(), 6000);
+    setTimeout(function(){redirector()}, 4000);
 })
