e1793a60e9d622b27e3967499b8121317402c125
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-27 21:03:38 -0800

    finished functionality work on frontend

--- js/Labs/axios_v2.js
+++ js/Labs/axios_v2.js
@@ -1,14 +1,49 @@
-// const url = 'https://jsonplaceholder.typicode.com/todos/1'
-const url = 'https://favqs.com/api/quotes'
+// DO NOT PUSH API KEY TO GITHUB
+let quotes = []
+let userInput = prompt("Enter in a keyword to recieve a related QUOTE.")
+let div = document.getElementById("quote")
 
-axios.get(url, {
-    headers: {
-        'Authorization': `Token token="YOUR_API_KEY"`
+function getData(userInput) {
+    const url = 'https://favqs.com/api/quotes?filter=' + userInput
+    
+    axios.get(url, {
+        headers: {
+            'Authorization': `Token token="API KEY GOES HERE***"`
+        }
+    })
+    .then(request => {
+        getBody(request)
+    })
+    .catch(error => console.log(error))
+} 
+
+function getBody(request) {
+    for(let quote of request.data.quotes) {
+        let addBody = quote.body
+        quotes.push(addBody)
     }
-})
-.then(request => {
-    // logic to work on data
-    console.log(request.data)
-})
-.catch(error => console.log(error))
+    div.textContent = quotes[Math.floor(Math.random() * quotes.length)]
+
+}
+getData(userInput)
+
+
+// document.createTextNode(userInput.value)
+
+// // V2
+// function http_get(url, success) {
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState === 1) {
+//             xhttp.setRequestHeader('Authorization', 'Token token="<api kep here>"')
+//         } else if (this.readyState === 4 && this.status === 200) {
+//             let data = JSON.parse(xhttp.responseText);
+//             success(data);
+//         } else if (this.readyState === 4 && this.status === 404) {
+//             // handle 404
+//         }
+//     };
+//     xhttp.open("GET", url);
+//     xhttp.send();
+// }
 
