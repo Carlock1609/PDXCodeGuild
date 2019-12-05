bb1117a8d21c9e3862fa275afeedba45ddce041d
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-12-05 00:13:58 -0800

    for in loop will work. Work on getting each ele

--- js/Labs/review.js
+++ js/Labs/review.js
@@ -1,37 +1,51 @@
-const url = "https://randomuser.me/api/"
+const url = "https://randomuser.me/api/";
 
 function getData() {
 axios.get(url)
-    .then(request => { console.log(request.data)
-        document.querySelector("#name").innerText += ` ${request.data.results[0].name.first} ${request.data.results[0].name.last}`
-        document.querySelector("#username").innerText += ` ${request.data.results[0].login.username}`
-        document.querySelector("#email").innerText += ` ${request.data.results[0].email}`
-        document.querySelector("#phone").innerText += ` ${request.data.results[0].phone}`
-        document.querySelector("#street").innerText += ` ${request.data.results[0].location.street.number} ${request.data.results[0].location.street.name}`
-        document.querySelector("#city").innerText += ` ${request.data.results[0].location.city}`
-        document.querySelector("#state").innerText += ` ${request.data.results[0].location.state}`
-        document.querySelector("#zip").innerText += ` ${request.data.results[0].location.postcode}`
+    .then(request => { console.log(request.data);
+        document.querySelector("#name").innerText += ` ${request.data.results[0].name.first} ${request.data.results[0].name.last}`;
+        document.querySelector("#username").innerText += ` ${request.data.results[0].login.username}`;
+        document.querySelector("#email").innerText += ` ${request.data.results[0].email}`;
+        document.querySelector("#phone").innerText += ` ${request.data.results[0].phone}`;
+        document.querySelector("#street").innerText += ` ${request.data.results[0].location.street.number} ${request.data.results[0].location.street.name}`;
+        document.querySelector("#city").innerText += ` ${request.data.results[0].location.city}`;
+        document.querySelector("#state").innerText += ` ${request.data.results[0].location.state}`;
+        document.querySelector("#zip").innerText += ` ${request.data.results[0].location.postcode}`;
     })
     .catch(error => console.log(error))
 };
 
 function moveCookies() {
-    let oldCookies = document.querySelector("#cookie-jar").textContent
-    let newCookies = document.querySelector("#newJar")
+    let oldCookies = document.querySelector("#cookie-jar").textContent;
+    let newCookies = document.querySelector("#newJar");
 
-    newCookies.textContent = oldCookies
-    document.querySelector("#cookie-jar").remove()
+    newCookies.textContent = oldCookies;
+    document.querySelector("#cookie-jar").remove();
 }
 function makePretty() {
-    let pretty = document.querySelector("#pretty").textContent
-    for(let ele of pretty) {
-        ele.style.color = "blue"
-    }
+    let strings = document.querySelector("#pretty").textContent;
+    let r = Math.floor(Math.random() * 255)
+    let g = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+    console.log(strings[])
+    for(let ele of strings) {
+        document.querySelector("#pretty").style.color = `rgb(${r}, ${g}, ${b})`
+        }
+    
+    
+    // for(let ele of strings) {
+    //     ele.style.color = "red"
+    // }
+
+
+        // ele.style.color = "blue"
+    
 }
 
 function main() {
-    getData()
-    moveCookies()
-    makePretty()
+    getData();
+    moveCookies();
+    makePretty();
 }
-main()
+main();
+
