a22a98203bd9671960c8848def463a817157b830
Jon <53983746+Carlock1609@users.noreply.github.com>
2020-01-23 15:13:21 -0800

    testing out message app

--- Django/TwitterAllauth_2.0/mysite/urls.py
+++ Django/TwitterAllauth_2.0/mysite/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('inbox/', include('inbox.urls')),
     path('', views.Home.as_view(), name='home'),
 ]
