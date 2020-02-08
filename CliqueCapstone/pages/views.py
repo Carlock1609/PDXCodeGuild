b93ca6e2d7714ec3a86c475382fafa64393ab448
Jon <53983746+Carlock1609@users.noreply.github.com>
2020-02-07 17:37:23 -0800

    Good work done on filter, figure otu bug

--- CliqueCapstone/pages/views.py
+++ CliqueCapstone/pages/views.py
@@ -28,37 +28,56 @@ def about(request):
 
 # ADD USERNAME TO SEARCH
 def search(request):
-    query = request.GET.get('q')
-    if query == None:
-        return render(request, 'pages/search.html')
-    else:
-        # THIS WORKS BUT FIGURE OUT HOW THE FLOW WOULD WORK
-        # Q(follower_amount__gte=query) AND Q(follower_amount__lte=query)
-        # Dataset.objects.filter(i_begin_int__lte=170, i_end_int__gte=170)
-        # Item.objects.filter(price__range=(min_price, max_price))
-
-        # number1 = request.FILES.get('number1')
-        # number2 = request.FILES.get('number2')
-        # number3 = request.FILES.get('number3')
-        # number4 = request.FILES.get('number4')
-        # number5 = request.FILES.get('number5')
-
-        # follower_filter = UserProfile.objects.filter(Q(follower_amount__lte=num1) & Q(follower_amount__gte=num2))
-
-        # follower_filter = UserProfile.objects.filter(Q(follower_amount__icontains=lte_filter) & Q(follower_amount__icontains=gte_filter))
-        results = UserProfile.objects.filter(Q(follower_amount__icontains=query) | Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query))
-        
-        context = {
-            'results': results,
-        }
-
-        return render(request, 'pages/search.html', context)
+    if request.method == 'GET':
+        query = request.GET.get('q')
+        follower = request.GET.get('follower_range')
 
 
-f request.method == 'POST':
-        # print(request.FILES)
-        image_file = request.FILES.get('image_file')
-        image_type = request.POST.get('image_type')
+        if query == None and follower == None:
+            return render(request, 'pages/search.html')
+        else:
+            # THIS WORKS BUT FIGURE OUT HOW THE FLOW WOULD WORK
+            # Q(follower_amount__gte=query) AND Q(follower_amount__lte=query)
+            # Dataset.objects.filter(i_begin_int__lte=170, i_end_int__gte=170)
+            # Item.objects.filter(price__range=(min_price, max_price))
+
+            # if follower == 0:
+            #     results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__gte=follower)
+
+            if follower == 0:
+                results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__gte=follower)
+            elif follower == 100:
+                results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__lte=follower,)
+            elif follower == 1000:
+                results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__lte=follower, follower_range__gte=follower-900)
+            elif follower == 5000:
+                results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__lte=follower, follower_range__gte=follower-4000)
+            elif follower == 10000:
+                results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__lte=follower, follower_range__gte=follower-5000)
+            elif follower == 50000:
+                results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__lte=follower, follower_range__gte=follower-40000)
+            elif follower == 100000:
+                results = UserProfile.objects.filter(Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query), follower_range__lte=follower, follower_range__gte=follower-50000)
+            
+
+            # follower_filter = UserProfile.objects.filter(Q(follower_amount__lte=num1) & Q(follower_amount__gte=num2))
+
+            # follower_filter = UserProfile.objects.filter(Q(follower_amount__icontains=lte_filter) & Q(follower_amount__icontains=gte_filter))
+            # results = UserProfile.objects.filter(Q(follower_amount__icontains=query) | Q(first_name__icontains=query) | Q(location__icontains=query) | Q(user__username=query) | Q(user__username__startswith=query))
+
+            
+            context = {
+                'results': results,
+            }
+
+            return render(request, 'pages/search.html', context)
+
+
+
+# f request.method == 'POST':
+#         # print(request.FILES)
+#         image_file = request.FILES.get('image_file')
+#         image_type = request.POST.get('image_type')
 
 # if request.method == 'GET':
 #     context = {
